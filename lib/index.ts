b24f40c47e5e36582545ee6e7ee00919125eac22
Guido Kessels <guidokessels@gmail.com>
2020-06-21 13:17:14 +0200

    WIP ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,71 @@
-const VERSION = '2.0.0';
+type XWS = {
+  description?: string;
+  faction: string;
+  name: string;
+  points?: number;
+  pilots: Array<XWSPilot>;
+  vendor: XWSVendor;
+  version: string;
+};
 
-const validateMap = map => {
+type XWSVendor = {
+  [key: string]: {
+    builder: string;
+    builder_url: string;
+    link: string;
+  };
+};
+
+type XWSSlot = {
+  [key: string]: Array<string>;
+};
+
+type XWSPilot = {
+  id: string;
+  ship?: string;
+  points?: number;
+  upgrades: XWSSlot;
+};
+
+type FFG2XWSMap = {
+  pilots: { [key: string]: string };
+  upgrades: { [key: string]: string };
+  slots: { [key: string]: string };
+  factions: { [key: string]: string };
+  ships: { [key: string]: string };
+};
+
+type FFGPilot = {
+  id: number;
+  name: string;
+  ship_type: number;
+};
+
+type FFGUpgrade = {
+  id: number;
+  name: string;
+  upgrade_types: Array<number>;
+};
+type FFGShip = {
+  pilot_card: FFGPilot;
+  slots: Array<FFGUpgrade>;
+  cost: number;
+};
+
+type FFGFaction = {
+  id: number;
+};
+
+type FFGSquad = {
+  id: string;
+  name: string;
+  description: string;
+  deck: Array<FFGShip>;
+  faction: FFGFaction;
+  cost: number;
+};
+
+const validateMap = (map: FFG2XWSMap) => {
   if (
     typeof map !== 'object' ||
     typeof map.pilots !== 'object' ||
@@ -12,7 +77,7 @@ const validateMap = map => {
   }
 };
 
-const validateSquad = ffgSquad => {
+const validateSquad = (ffgSquad: FFGSquad) => {
   if (typeof ffgSquad !== 'object') {
     throw new Error('Invalid squad');
   }
@@ -24,7 +89,7 @@ const validateSquad = ffgSquad => {
   }
 };
 
-const createVendor = id => ({
+const createVendor = (id: string): XWSVendor => ({
   ffg: {
     builder: 'FantasyFlightGames Squadbuilder',
     builder_url: 'https://squadbuilder.fantasyflightgames.com',
@@ -32,14 +97,14 @@ const createVendor = id => ({
   },
 });
 
-const convert = (ffgSquad, xwsMap) => {
+const convert = (ffgSquad: FFGSquad, xwsMap: FFG2XWSMap): XWS => {
   validateMap(xwsMap);
   validateSquad(ffgSquad);
 
-  const { faction, id = '', description = '', name = '', deck = [], cost = 0 } = ffgSquad;
+  const { faction, id, description, name, deck, cost } = ffgSquad;
 
-  const pilots = deck.map(({ pilot_card = {}, slots = [], cost = 0 }, index) => {
-    const { id: pilotId, name: pilotName = '', ship_type } = pilot_card;
+  const pilots = deck.map(({ pilot_card, slots, cost: points }, index) => {
+    const { id: pilotId, name: pilotName, ship_type } = pilot_card;
     const xwsId = xwsMap.pilots[pilotId];
     if (!xwsId) {
       throw new Error(
@@ -47,7 +112,7 @@ const convert = (ffgSquad, xwsMap) => {
       );
     }
     const ship = xwsMap.ships[ship_type];
-    const upgrades = {};
+    const upgrades: XWSSlot = {};
     slots.forEach(({ upgrade_types, id: upgradeId, name: upgradeName = '' }) => {
       const slotId = upgrade_types[0];
       const slot = xwsMap.slots[slotId];
@@ -66,18 +131,15 @@ const convert = (ffgSquad, xwsMap) => {
       upgrades[slot].push(upgradeXwsID);
     });
 
-    const result = {
+    const result: XWSPilot = {
       id: xwsId,
       upgrades,
-      points: cost,
-      ship: undefined,
+      points,
     };
 
     // Ship is optional; Add it if we can find it, but don't break if we can't
     if (ship) {
       result.ship = ship;
-    } else {
-      delete result.ship;
     }
 
     return result;
@@ -90,7 +152,7 @@ const convert = (ffgSquad, xwsMap) => {
     points: cost,
     faction: xwsMap.factions[faction.id],
     vendor: createVendor(id),
-    version: VERSION,
+    version: '2.0.0',
   };
 
   return xws;
